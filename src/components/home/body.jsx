c9add5f3212438c937af770ad9ecaa4aa3bb28de
Sardar-Ali-Murad <mar389786@gmail.com>
2025-01-17 18:50:26 +0500

    changes

--- src/components/home/body.jsx
+++ src/components/home/body.jsx
@@ -4,19 +4,24 @@ import computer from "../../assets/home/computer.svg";
 
 const body = () => {
   return (
-    <div className="home-body-wrap">
-      <div className="body-text-wrap">
-        <p>
-          TaxReady.co.uk is specifically designed for UK small businesses,
-          offering streamlined tax filing solutions. Accessible through desktop
-          and mobile applications, it provides a convenient way to manage your
-          tax responsibilities with HMRC.
-        </p>
-        <p>Complete your taxes within minutes not hours.</p>
+    <div className="home-body-main-wrap">
+      <div className="home-body-wrap">
+        <div className="body-text-wrap">
+          <p>
+            TaxReady.co.uk is specifically designed for UK small businesses,
+            offering streamlined tax filing solutions. Accessible through
+            desktop and mobile applications, it provides a convenient way to
+            manage your tax responsibilities with HMRC.
+          </p>
+          <p>Complete your taxes within minutes not hours.</p>
+        </div>
+        <div className="body-img-wrap">
+          <img src={mobile} className="mobile-img" />
+          <img src={computer} className="computer-img" />
+        </div>
       </div>
-      <div className="body-img-wrap">
-        <img src={mobile} className="mobile-img" />
-        <img src={computer} className="computer-img" />
+      <div className="footer-text-wrap">
+        <p className="footer-text">© 2024 Snapfile. All rights reserved.</p>
       </div>
     </div>
   );
