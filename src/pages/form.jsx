7e1fb3bec3c18dc6dda5d417c1c5289035fb7a1f
Sardar-Ali-Murad <mar389786@gmail.com>
2025-01-21 22:31:29 +0500

    changes

--- src/pages/form.jsx
+++ src/pages/form.jsx
@@ -10,6 +10,7 @@ import ReportingPeriodCard from "../components/form/reporting-period-carrd";
 import BusinessTypeCard from "../components/form/business-type-card";
 import ExpensesCard from "../components/form/expenses-card";
 import FinishedCard from "../components/form/finishes-card";
+import ReviewCard from "../components/form/review-card";
 
 import "./index.css";
 
@@ -111,6 +112,20 @@ const Form = () => {
             { name: "pay", step: 8, disabled: true },
             { name: "login", step: 9, disabled: true },
             { name: "done", step: 10, disabled: false },
+          ]}
+        />
+      )}
+      {count === 10 && (
+        <ReviewCard
+          setCount={setCount}
+          routes={[
+            { name: "start", step: 1, disabled: true },
+            { name: "HMRC", step: 2, disabled: true },
+            { name: "verify", step: 3, disabled: true },
+            { name: "reporting period", step: 4, disabled: true },
+            { name: "type", step: 5, disabled: true },
+            { name: "income/ expenses", step: 6, disabled: true },
+            { name: "review", step: 7, disabled: false },
           ]}
         />
       )}
