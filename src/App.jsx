41d39e5403490e2d344925ed4eef12213bc4f617
Sardar-Ali-Murad <mar389786@gmail.com>
2025-01-22 17:39:13 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
-import UserInfo from "./pages/user-info";
 import Form from "./pages/form";
 
 const App = () => {
@@ -11,7 +10,6 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/user-info" element={<UserInfo />} />
           <Route path="/form" element={<Form />} />
         </Routes>
       </BrowserRouter>
