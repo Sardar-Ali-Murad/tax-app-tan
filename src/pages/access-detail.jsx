6d31435da56898c054960482243d3b963aa63e0c
Sardar-Ali-Murad <mar389786@gmail.com>
2025-03-10 21:38:53 +0500

    changes

--- src/pages/access-detail.jsx
+++ src/pages/access-detail.jsx
@@ -6,32 +6,72 @@ import axios from "axios";
 import { BASE_URL } from "../config/constants";
 import { useLocation } from "react-router-dom";
 import { handleSetToken } from "../global-redux/reducers/auth/slice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./index.css";
 
 const AccessDetails = () => {
   const dispatch = useDispatch();
   const location = useLocation();
+  const { user } = useSelector((state) => state?.auth);
+
   React.useEffect(() => {
-    if (location?.search) {
-      sessionStorage.setItem("code", location?.search?.split("=")[1]);
-      const getToken = async () => {
+    if (!location?.search) return;
+
+    const code = location.search.split("=")[1];
+    if (!code) return;
+
+    sessionStorage.setItem("code", code);
+
+    const getToken = async () => {
+      try {
         const response = await axios.get(
-          `${BASE_URL}/api/external/redirect?code=${
-            location?.search?.split("=")[1]
-          }`,
+          `${BASE_URL}/api/external/redirect?code=${code}`,
           {
             headers: {
               "ngrok-skip-browser-warning": "true",
             },
           }
         );
-        dispatch(handleSetToken(response?.data));
-        sessionStorage.setItem("token", response?.data);
-      };
-      getToken();
-    }
-  }, [location]);
+
+        const token = response?.data;
+        dispatch(handleSetToken(token));
+        sessionStorage.setItem("token", token);
+
+        if (!user?.nino) {
+          console.error("User NINO is missing.");
+          return;
+        }
+
+        // First API call
+        const firstResponse = await axios.get(
+          `${BASE_URL}/api/external/individualCalculationsGetId?nino=${user.nino}&token=${token}&taxYear=2024-25&calculationType=in-year`,
+          {
+            headers: {
+              "ngrok-skip-browser-warning": "true",
+            },
+          }
+        );
+
+        if (firstResponse.status === 200 || firstResponse.status === 201) {
+          // Second API call
+          const secondResponse = await axios.get(
+            `${BASE_URL}/api/external/individualCalculations?nino=${user.nino}&token=${token}&taxYear=2024-25&calculationId=${firstResponse?.data?.calculationId}`,
+            {
+              headers: {
+                "ngrok-skip-browser-warning": "true",
+              },
+            }
+          );
+          console.log("Second API response:", secondResponse?.data);
+        }
+      } catch (error) {
+        console.error("Error fetching token or calculation data:", error);
+      }
+    };
+
+    getToken();
+  }, [location?.search]);
+
   return (
     <div>
       <Header />
